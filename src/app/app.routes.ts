f21bcdbc165f448e26b88fd03f0196bd78deb555
hussienokasha <hussien415532@gmail.com>
2025-03-15 11:47:03 +0200

    Remove unused authentication component and add user component with routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,26 @@
 import { Routes } from '@angular/router';
-
+import { DashboardComponent } from './Layouts/dashboard/dashboard.component';
+import { authGuard } from './Core/guards/auth.guard';
+import { redirectGuard } from './Core/guards/redirect.guard';
+import { UserComponent } from './Layouts/user/user.component';
 
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: '',
+    canActivate: [redirectGuard],
+    component: UserComponent,
+    loadChildren: () =>
+      import('./Layouts/user/user.routes').then((m) => m.routes),
+  },
+  {
+    path: 'dashboard',
+    canActivate: [authGuard],
+    component: DashboardComponent,
+    loadChildren: () =>
+      import('./Layouts/dashboard/dashboard.routes').then((m) => m.routes),
+  },
 
+  { path: '**', redirectTo: '' },
 ];
