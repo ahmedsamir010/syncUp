ee828509d1feeb0a7645cd2350a3caf5fce11ab1
hussienokasha <hussien415532@gmail.com>
2025-03-15 11:53:19 +0200

    Implement routing for dashboard and user components, add home pages

--- src/app/Layouts/dashboard/dashboard.routes.ts
+++ src/app/Layouts/dashboard/dashboard.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-
-
+import { HomeComponent } from '../../Features/dashboard/pages/home/home.component';
 
 export const routes: Routes = [
-{path:'',redirectTo:'home'}
+  { path: '', redirectTo: 'home' ,pathMatch:'full'},
+  { path: 'home', component: HomeComponent },
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,23 +4,21 @@ import { authGuard } from './Core/guards/auth.guard';
 import { redirectGuard } from './Core/guards/redirect.guard';
 import { UserComponent } from './Layouts/user/user.component';
 
-
 export const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   {
     path: '',
-    canActivate: [redirectGuard],
+
     component: UserComponent,
     loadChildren: () =>
       import('./Layouts/user/user.routes').then((m) => m.routes),
   },
   {
     path: 'dashboard',
-    canActivate: [authGuard],
+
     component: DashboardComponent,
     loadChildren: () =>
       import('./Layouts/dashboard/dashboard.routes').then((m) => m.routes),
   },
 
-  { path: '**', redirectTo: '' },
+  
 ];
