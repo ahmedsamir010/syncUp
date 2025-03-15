6badf2b27756cf4f52306acca2cd9a63b36a5cc5
hussienokasha <hussien415532@gmail.com>
2025-03-15 13:26:59 +0200

    Add footer component to home page and update layout

--- src/app/Features/user/pages/home/home.component.ts
+++ src/app/Features/user/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../../../Shared/components/header/header.component";
+import { FooterComponent } from "../../../../Shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
