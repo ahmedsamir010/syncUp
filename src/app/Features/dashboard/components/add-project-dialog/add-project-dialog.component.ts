473e14ff6b7913206fd86e0819ddd839fc5edc61
ahmedsamir010 <ahmed0x0samir@gmail.com>
2025-03-15 15:44:12 +0200

    Add meetings page with routing, layout, and expert list functionality

---
+++ src/app/Features/dashboard/components/add-project-dialog/add-project-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-project-dialog',
+  imports: [],
+  templateUrl: './add-project-dialog.component.html',
+  styleUrl: './add-project-dialog.component.scss'
+})
+export class AddProjectDialogComponent {
+
+}
--- src/app/Features/dashboard/pages/projects/projects.component.ts
+++ src/app/Features/dashboard/pages/projects/projects.component.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  tabs = ['All', 'Active', 'Upcoming', 'Completed'];
+  filter: string = 'All';
 
+  projects = [
+    { title: 'Web Development', status: 'Active', dueDate: 'Due 30/11/2024', tasks: 8, progress: 50, borderColor: 'border-purple-600' },
+    { title: 'Mobile App Design', status: 'Active', dueDate: 'Due 30/11/2024', tasks: 8, progress: 50, borderColor: 'border-teal-600' },
+    { title: 'Web Development', status: 'Upcoming', dueDate: 'Due 30/11/2024', tasks: 5, progress: 20, borderColor: 'border-orange-400' },
+    { title: 'Web Development', status: 'Completed', dueDate: 'Due 30/11/2024', tasks: 8, progress: 90, borderColor: 'border-red-500' }
+  ];
+
+  filteredProjects = [...this.projects];
+
+  setFilter(tab: string) {
+    this.filter = tab;
+    if (tab === 'All') {
+      this.filteredProjects = [...this.projects];
+    } else {
+      this.filteredProjects = this.projects.filter(project => project.status === tab);
+    }
+  }
 }
